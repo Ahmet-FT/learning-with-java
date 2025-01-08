53016f05bc1f1122816291b7710fb20ab2d9886c
Ahmet-FT <ahmetf.tekin@gmail.com>
2025-01-09 01:56:12 +0300

    prep for finale

---
+++ FinalePrep/ExecptionHandle.java
@@ -0,0 +1,45 @@
+
+public class ExceptionHandle {
+    /*istisnai bir durrum ortaya çıktığında,
+     o istisnayı temsil eden bir nesen yaratılır ve hataya sebep olan metodun içine atılır(throw)
+     
+     programcının olaya nasıl bakması herektiğiyle ilgili olarak bu metot istisnayı yakalar
+     (catch) ve işler ya da yakalnıp işlenmesi için istisnaya dokunmaz geçer.*/
+
+
+    public void setRadius() throws Exception{
+        try {
+            throw new Exception("this is an exception");
+        } catch(Exception e) {
+            System.out.println("setRadius() exeption handled: " + e);
+            throw e;
+        }
+     }
+
+      public void setRadius2() throws Exception {
+        try {
+            setRadius();
+        } catch (Exception e) {
+            System.out.println("setRadius() exeption handled: " + e);
+            throw e;
+        }
+     }
+
+     public void start() {
+        try {
+
+            setRadius2();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+     }
+     public static void main(String[] args) {
+        ExceptionHandle f = new ExceptionHandle();
+        
+        f.start();
+
+    }
+}
